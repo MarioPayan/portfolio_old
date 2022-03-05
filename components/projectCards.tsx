e17170495ea0650369a64aef0c0b7c7df8484883
MarioPayan <mpayan.av@gmail.com>
2022-03-05 17:36:48 -0500

    Adding see more and see less options

--- components/projectCards.tsx
+++ components/projectCards.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {ProjectCards as ProjectCardsType, Project} from '../types/types'
 import {getKeyFromLabel, openInNewTab} from '../utils/utils'
 import {image404} from '../utils/assets'
@@ -9,12 +9,59 @@ import {
   CardContent,
   Typography,
   CardActions,
-  Button
+  Button,
+  Collapse,
+  Link
 } from '@mui/material'
 import Lock from '@mui/icons-material/Lock'
 import Context from './context'
 
 const ProjectCards: ProjectCardsType = () => {
+  const [seeMore, setSeeMore] = useState<any>({})
+
+  const getTextVersion = (text: string, key: string, t: any) => {
+    const TextComponent = (component: JSX.Element) => (
+      <Typography variant="body2" color="text.secondary">
+        {component}
+      </Typography>
+    )
+
+    const LinkComponent = () => (
+      <Link
+        underline="none"
+        sx={{cursor: 'pointer'}}
+        color="secondary"
+        onClick={() => {
+          setSeeMore({...seeMore, [key]: !seeMore[key]})
+        }}>
+        {t(`misc.label.${seeMore[key] ? 'seeLess' : 'seeMore'}`)}
+      </Link>
+    )
+
+    const maxLength = 55
+    if (text.length < maxLength) return TextComponent(<>{text}</>)
+    if (seeMore[key])
+      return TextComponent(
+        <>
+          {text + ' '}
+          {LinkComponent()}
+        </>
+      )
+    const textArray = text.split(' ')
+    let currentLength = 0
+    for (let i = 0; i < textArray.length; i++) {
+      if (textArray[i].length + currentLength > maxLength) {
+        return TextComponent(
+          <>
+            {textArray.splice(0, i + 1).join(' ') + '... '} {LinkComponent()}
+          </>
+        )
+      } else {
+        currentLength += textArray[i].length
+      }
+    }
+  }
+
   return (
     <Context.Consumer>
       {({t}) => (
@@ -28,30 +75,29 @@ const ProjectCards: ProjectCardsType = () => {
           {(t('projects', {returnObjects: true}) as unknown as Project[]).map(
             project => (
               <Grid item key={getKeyFromLabel(project.label)}>
-                <Card sx={{width: 345, height: 300}}>
+                <Card sx={{width: 345}}>
                   <CardMedia
                     component="img"
                     height="140"
                     image={project.image ? project.image : image404}
                     alt={`${project.label}`}/>
-                  <CardContent>
+                  <CardContent sx={{pb: 1}}>
                     <Typography gutterBottom variant="h5" component="div">
                       {project.label}
                     </Typography>
-                    <Typography
-                      variant="body2"
-                      color="text.secondary"
-                      sx={{
-                        display: '-webkit-box',
-                        WebkitBoxOrient: 'vertical',
-                        WebkitLineClamp: 2,
-                        overflow: 'hidden',
-                        height: 35,
-                      }}>
-                      {project.description}
-                    </Typography>
+                    <Collapse
+                      orientation="vertical"
+                      in={seeMore[getKeyFromLabel(project.label)] || false}
+                      collapsedSize={40}>
+                      {getTextVersion(
+                        project.description,
+                        getKeyFromLabel(project.label),
+                        t
+                      )}
+                    </Collapse>
                   </CardContent>
-                  <CardActions>
+                  <CardActions
+                    sx={{display: 'flex', justifyContent: 'flex-end'}}>
                     <Button
                       disabled={!project.public}
                       variant="contained"
